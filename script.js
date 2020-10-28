7741562d3b8d510501862f0ba0268ddd66be2e91
AsafDafna <55590136+AsafDafna@users.noreply.github.com>
2020-10-28 12:59:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const tags = ['c', 'qt', 'obj']; // these are my tags: comment, blockquote, and objection. Use whatever you want and then update the CSS style accordingly
+const tag = 'RTL'; // these are my tags: comment, blockquote, and objection. Use whatever you want and then update the CSS style accordingly
 
-function applyAndHideTag(tags) {
-    tags.forEach((tag) => {
-        document.querySelectorAll('span[data-tag=' + tag + ']').forEach((node) => {
-            if (node.closest('div.roam-block-container').classList.contains(tag + '_tag') == false) { // this check is so that this function only runs when necessary and doesn't cause the mutationObserver to go into an infinite loop
-                node.closest('div.roam-block-container').classList.add(tag + '_tag');
-                node.parentNode.innerHTML = node.parentNode.innerHTML.replace('</span> ', '</span>'); //since the custom CSS will hide the tag, this gets ready of the resulting leading whitespace
-            }
-        });
-    });
-}
+function applyAndHideTag(tag) {
+  document.querySelectorAll('span[data-tag=' + tag + ']').forEach((node) => {
+      if (node.closest('div.roam-block-container').classList.contains(tag + '_tag') == false) { // this check is so that this function only runs when necessary and doesn't cause the mutationObserver to go into an infinite loop
+          node.closest('div.roam-block-container').classList.add(tag + '_tag');
+          node.parentNode.innerHTML = node.parentNode.innerHTML.replace('</span> ', '</span>'); //since the custom CSS will hide the tag, this gets ready of the resulting leading whitespace
+      }
+  })
+};
 
-const mutationObserver = new MutationObserver(() => applyAndHideTag(tags))
+const mutationObserver = new MutationObserver(() => applyAndHideTag(tag))
 
 const config = {
     childList: true,
